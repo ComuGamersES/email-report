88b0223673d3fd00f380e8808929e0ed74fe3e61
Pabszito <pabszito@gmail.com>
2022-06-29 00:46:40 -0300

    chore: initial commit
    create project

---
+++ README.md
@@ -0,0 +1,4 @@
+## Sentey Email Abuse Reports
+because this right here isn't enough:
+
+![img.png](img/img.png)
---
+++ src/main/java/com/comugamers/sentey/addon/email/EmailReports.java
@@ -0,0 +1,12 @@
+package com.comugamers.sentey.addon.email;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EmailReports extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
